a2cb8c71c3f377a472028d83e705fb3437eea8a1
tonyscan6003 <58773639+tonyscan6003@users.noreply.github.com>
2021-10-07 17:10:01 +0100

    Update helper.py

--- python/helper.py
+++ python/helper.py
@@ -83,3 +83,33 @@ def _scale_boxes(boxes, ss_asp_ratio, ls_asp_ratio, ss_frac_offset, ls_frac_offs
         r4 = (ss_asp_ratio) * boxes[..., 3] + ss_frac_offset
         scale_boxes = tf.transpose(tf.stack([r1, r2, r3, r4], axis=0))
     return scale_boxes
+
+def display_image(train_dataset):
+    cntr = 0
+    for img, boxes, obj_cen, labels in train_dataset.take(1):
+        H_val = np.shape(img)[0]
+        W_val = np.shape(img)[1]
+        img = img.to_tensor(shape=[batch_size, HW_trg, HW_trg, 3])
+        boxes = boxes[0]
+        obj_cen = obj_cen[0]
+        new_img = np.asarray(unprocess_image(img[0, :, :, :]))
+        
+        i = 0
+        for b in boxes:
+            print(b)
+            print(labels[i])
+            # get the bounding rects from dataset
+            ymin = tf.cast((b[0] * HW_trg), tf.int32)
+            xmin = tf.cast((b[1] * HW_trg), tf.int32)
+            ymax = tf.cast((b[2] * HW_trg), tf.int32)
+            xmax = tf.cast((b[3] * HW_trg), tf.int32)
+            # print(xmin,xmax,ymin,ymax)
+            # draw a green rectangle to visualize the bounding rect
+            centre_int = (HW_trg * obj_cen)
+            centre_int = tf.cast((HW_trg * obj_cen[i, :]), tf.int32)
+            new_img = cv2.rectangle((new_img), (xmin, ymin), (xmax, ymax), (0, 255, 0), 2)
+            new_img = cv2.circle(new_img, (centre_int[1], centre_int[0]), 4, (255, 0, 0), 2)
+            i += 1
+        cntr = cntr + 1
+        print(cntr)
+    plt.imshow(new_img)
