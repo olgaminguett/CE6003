dc535436779da87de2e8ea27f22f7134ac2c9a0a
tonyscan6003 <58773639+tonyscan6003@users.noreply.github.com>
2021-10-07 16:18:01 +0100

    Create helper.py

---
+++ python/helper.py
@@ -0,0 +1,84 @@
+import tensorflow as tf
+from tensorflow import keras
+import tensorflow_addons as tfa
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd 
+import cv2
+import glob
+import datetime
+
+def process_image(input_img):
+    input_img = tf.cast(input_img, tf.float32)
+    # op_img = tf.keras.applications.mobilenet_v2.preprocess_input(
+    # input_img, data_format=None)
+    op_img = tf.keras.applications.vgg16.preprocess_input(
+        input_img, data_format=None)
+    return op_img
+
+
+def unprocess_image(input_img):
+    [0.485, 0.456, 0.406]
+    b = tf.cast(input_img[:, :, 0] + tf.math.round(0.406 * 255), tf.int32)
+    g = tf.cast(input_img[:, :, 1] + tf.math.round(0.456 * 255), tf.int32)
+    r = tf.cast(input_img[:, :, 2] + tf.math.round(0.485 * 255), tf.int32)
+    op_img = tf.stack([r, g, b], axis=2)
+    return op_img
+  
+def scale_pad(ip_image, boxes, labels):
+
+    ip_size = tf.cast(tf.shape(ip_image)[:-1], tf.float32)
+    in_max = tf.math.argmax(ip_size)
+    in_min = tf.math.argmin(ip_size)
+    sf = HW_trg / ip_size[in_max]
+    # Rescale the image (same ratio both sides)
+    op_size = tf.cast(tf.math.round(sf * ip_size), tf.int32)
+    ip_resize = tf.image.resize(ip_image, op_size, method=tf.image.ResizeMethod.BILINEAR)
+    # Translate image within nominal HW_trg x HW_trg  window and add padding.
+    # Translate shorter size image with padding operation.
+    trans_ss_sf = tf.random.uniform(shape=[], minval=0.1, maxval=0.9)
+    delta_ss = tf.cast(HW_trg - op_size[in_min], tf.float32)
+    ss_offset_u = tf.cast(tf.math.floor(trans_ss_sf * delta_ss), tf.int32)
+    ss_offset_l = HW_trg - (op_size[in_min] + ss_offset_u)
+    # Translate longer size image with padding operation
+    trans_ls_sf = tf.random.uniform(shape=[], minval=0.1, maxval=0.9)
+    delta_ls = tf.cast(HW_trg - op_size[in_max], tf.float32)
+    ls_offset_u = tf.cast(tf.math.floor(trans_ls_sf * delta_ls), tf.int32)
+    ls_offset_l = HW_trg - (op_size[in_max] + ls_offset_u)
+    if in_max == 1:
+        paddings = [[ss_offset_u, ss_offset_l], [ls_offset_u, ls_offset_l], [0, 0]]
+    else:
+        paddings = [[ls_offset_u, ls_offset_l], [ss_offset_u, ss_offset_l], [0, 0]]
+    ip_resize = tf.pad(ip_resize, paddings, "CONSTANT")
+    # ip_resize = tf.image.rot90(ip_resize, k=1)
+    # Rescale + offset all bounding box coordinates to match image transformations
+    #
+    ss_frac_offset = tf.cast(ss_offset_u / HW_trg, tf.float32)
+    ls_frac_offset = tf.cast(ls_offset_u / HW_trg, tf.float32)
+    ls_asp_ratio = tf.cast(1.0, tf.float32)
+    ss_asp_ratio = tf.cast(ip_size[in_min] / ip_size[in_max], tf.float32)
+    scale_boxes = _scale_boxes(boxes, ss_asp_ratio, ls_asp_ratio, ss_frac_offset, ls_frac_offset, in_max)
+
+    # Output object centres (This is needed for yolo training)
+    i_centre = [scale_boxes[..., 2] / 2 + scale_boxes[..., 0] / 2]
+    j_centre = [scale_boxes[..., 3] / 2 + scale_boxes[..., 1] / 2]
+    object_centres = tf.transpose(tf.concat((i_centre, j_centre), axis=0))
+
+    return ip_resize, scale_boxes, object_centres, labels
+
+# Obtain tensor slices from boxes scale add and conctenate portions (don't use mask (assignment) as in earlier versions)
+def _scale_boxes(boxes, ss_asp_ratio, ls_asp_ratio, ss_frac_offset, ls_frac_offset, in_max):
+    # Also correct for aspect ratio of smaller side and include offset
+    if in_max == 1:
+        r1 = (ss_asp_ratio) * boxes[..., 0] + ss_frac_offset
+        r2 = (ls_asp_ratio) * boxes[..., 1] + ls_frac_offset
+        r3 = (ss_asp_ratio) * boxes[..., 2] + ss_frac_offset
+        r4 = (ls_asp_ratio) * boxes[..., 3] + ls_frac_offset
+        scale_boxes = tf.transpose(tf.stack([r1, r2, r3, r4], axis=0))
+    else:
+        r1 = (ls_asp_ratio) * boxes[..., 0] + ls_frac_offset
+        r2 = (ss_asp_ratio) * boxes[..., 1] + ss_frac_offset
+        r3 = (ls_asp_ratio) * boxes[..., 2] + ls_frac_offset
+        r4 = (ss_asp_ratio) * boxes[..., 3] + ss_frac_offset
+        scale_boxes = tf.transpose(tf.stack([r1, r2, r3, r4], axis=0))
+    return scale_boxes
